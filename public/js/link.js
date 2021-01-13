6338305ba420c350db9e01848e5417456b81e5d3
yashwanthkkn <yashwanthkkn>
2021-01-13 23:59:29 +0530

    added email verification

--- public/js/link.js
+++ public/js/link.js
@@ -1,23 +1,17 @@
-// const shareData = {
-//     title: 'MDN',
-//     text: 'Learn web development on MDN!',
-//     url: 'https://developer.mozilla.org',
-//   }
-  
-//   const btn = document.querySelector('#share');
-  
-//   // Must be triggered some kind of "user activation"
-//   btn.addEventListener('click', async () => {
-//     try {
-//       await navigator.share(shareData)
-//     } catch(err) {
-//         console.log(err)
-//     }
-//   });
 
-function copyText(id){
-    
+
+async function copyText(id){
     var copyTextarea = document.getElementById("share");
+    const shareData = {
+        title: 'Funee.in',
+        text: 'Send Your Friend a Secret Message',
+        url: copyTextarea.value,
+    }
+    try {
+        await navigator.share(shareData)
+      } catch(err) {
+          console.log(err)
+    }
     copyTextarea.select(); 
     copyTextarea.setSelectionRange(0, 99999);
     document.execCommand("copy"); 
