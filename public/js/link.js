47c7819a8c60e13252a0a56ddd924e91c78deb32
yashwanthkkn <yashwanthkkn>
2021-01-15 13:05:52 +0530

    prevented default action on Enter

--- public/js/link.js
+++ public/js/link.js
@@ -1,4 +1,11 @@
-
+$(document).ready(function() {
+    $(window).keydown(function(event){
+      if(event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+      }
+    });
+  });
 
 async function copyText(id){
     var copyTextarea = document.getElementById("share");
